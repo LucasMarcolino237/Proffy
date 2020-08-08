22a349f3b9d9b72b5a982f32539f221d67cc2cde
Lucas Silva Marcolino <lukamarc.dev.@gmail.com>
2020-08-07 22:50:03 -0300

    Dia 5 - Next Level Week #2

--- src/server.js
+++ src/server.js
@@ -1,56 +1,4 @@
-const proffys = [
-    {
-        name: "Diego fernandes",
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v4",
-        whatsapp: "40028922", 
-        bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
-        subject: "Química", 
-        cost: "20", 
-        weekday: [], 
-        time_from: [], 
-        time_to: []
-    },
-    {
-        name: "Danielle Evangelista",
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v4",
-        whatsapp: "40028922", 
-        bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
-        subject: "Português",
-        cost: "20",
-        weekday: [],
-        time_from: [], 
-        time_to: []
-    }
-]
-
-const subjects = [
-    "Artes",
-    "Biologia",
-    "Ciências",
-    "Educação física",
-    "Física",
-    "Geografia",
-    "História",
-    "Matemática",
-    "Português",
-    "Química",
-]
-
-const weekday = [
-    "Domingo",
-    "Segunda-feira",
-    "Terça-feira",
-    "Quarta-feira",
-    "Quinta-feira",
-    "Sexta-feira",
-    "Sábado",
-]
-
-function getSubject(subjectNumber) {
-    const position = +subjectNumber - 1
-
-    return subjects[position]
-}
+const { pageLanding, pageStudy, pageGiveClasses, saveClasses } = require('./pages')
 
 const express = require('express')
 const server = express()
@@ -61,30 +9,13 @@ nunjucks.configure('src/views', {
     noCache: true
 })
 
-server.use(express.static("public"))
-.get("/", (req, res) => {
+server
+.use(express.urlencoded({ extended: true }))
+.use(express.static("public"))
 
-    return res.render("index.html")
-})
-.get("/study", (req, res) => {
-
-    const filters = req.query
-    
-    return res.render("study.html", { proffys, filters, subjects, weekday })
-})
-.get("/give-classes", (req, res) => {
-    const data = req.query
-    const isNotEmpty = Object.keys(data).length > 0
-
-    if (isNotEmpty) {
-
-        data.subject = getSubject(data.subject)
-        proffys.push(data)
-
-        return res.redirect("/study")
-    }
-    
-    return res.render("give-classes.html", { subjects, weekday })
-})
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
 
 .listen(5500)
